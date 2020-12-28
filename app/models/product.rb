2f9f37fa3397adce21fddeb98e34977e3e52388f
Djamal Sadaghiani <dj8amal@gmail.com>
2020-12-28 14:57:11 +0100

    finished feature completness to pyhton version

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,31 @@
-require 'net/http'
+require 'curb'
+require 'json'
 
 class Product < ApplicationRecord
   has_many :trades
-  after_create :get_ticker_by_isin
+  after_create :get_ticker_by_isin, :get_analyst_ratings
 
   def get_ticker_by_isin
-    uri = URI("https://query2.finance.yahoo.com/v1/finance/search?"
+    base_url = "https://query2.finance.yahoo.com/v1/finance/search?"
     params = {q: self.isin, quotesCount: 1, newsCount: 0}
-    uri.query = URI.encode_www_form(params)
-    res = Net::HTTP.get_response(uri)
-    puts res.body# if res.is_a?(Net::HTTPSuccess)
+    response = Curl.get(base_url, params)
+    json = JSON.parse(response.body_str)
+    self.ticker = json['quotes'][0]['symbol'] if json['quotes'][0]['symbol']
+    self.save
+  end
+
+  def get_stockdata_by_ticker
+    url = "https://query2.finance.yahoo.com/v10/finance/quoteSummary/#{self.ticker}?modules=financialData"
+    JSON.parse(Curl.get(url).body_str)
+  end
+
+  def get_analyst_ratings
+    data = get_stockdata_by_ticker()
+    return if data['quoteSummary']['result'].nil?
+    record = data['quoteSummary']['result'][0]['financialData']
+    self.recommendations = record['recommendationMean']['fmt']
+    self.mean_target_price = record['targetMeanPrice']['fmt']
+    self.number_of_analysts = record['numberOfAnalystOpinions']['fmt']
+    self.save
   end
 end
-
