7873d0e1b89e272aeed06e505dde4a1f57fa47fe
Jan-Dominik Oboth <jd.oboth@gmail.com>
2021-01-09 18:57:49 +0100

    implemented currency job and schedule

---
+++ db/migrate/20210109163153_create_currencies.rb
@@ -0,0 +1,11 @@
+class CreateCurrencies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :currencies do |t|
+      t.string :currency_ISO
+      t.float :exchange_rate_to_EUR
+
+      t.timestamps
+    end
+    add_index :currencies, :currency_ISO, unique: true
+  end
+end
