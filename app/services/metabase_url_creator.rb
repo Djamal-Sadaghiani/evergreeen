7fe607de4dec13220d1e65e87a07a33e11b5b3e5
Djamal Sadaghiani <dj8amal@gmail.com>
2021-01-13 20:36:52 +0100

    added price and volume charting

---
+++ app/services/metabase_url_creator.rb
@@ -0,0 +1,22 @@
+require 'jwt'
+
+METABASE_SITE_URL = "http://evergreeenreporting.herokuapp.com"
+
+class MetabaseUrlCreator < ApplicationService
+  def initialize(params)
+    @product_id = params[:product_id]
+  end
+
+  def call
+    payload = {
+      :resource => {:question => 12},
+      :params => {
+        "product_id" => @product_id.to_s
+      },
+      :exp => Time.now.to_i + (60 * 10) # 10 minute expiration
+    }
+    token = JWT.encode payload, ENV['METABASE_SECRET_KEY']
+
+    METABASE_SITE_URL + "/embed/question/" + token + "#bordered=true&titled=true"
+  end
+end
