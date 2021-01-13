b36b06f72d7c61239c9e92807f5b86929865eba2
Djamal Sadaghiani <dj8amal@gmail.com>
2021-01-13 20:37:33 +0100

    rubocop

--- app/services/metabase_url_creator.rb
+++ app/services/metabase_url_creator.rb
@@ -1,6 +1,6 @@
 require 'jwt'
 
-METABASE_SITE_URL = "http://evergreeenreporting.herokuapp.com"
+METABASE_SITE_URL = 'http://evergreeenreporting.herokuapp.com'
 
 class MetabaseUrlCreator < ApplicationService
   def initialize(params)
@@ -9,14 +9,14 @@ class MetabaseUrlCreator < ApplicationService
 
   def call
     payload = {
-      :resource => {:question => 12},
-      :params => {
-        "product_id" => @product_id.to_s
+      resource: { question: 12 },
+      params: {
+        'product_id' => @product_id.to_s
       },
-      :exp => Time.now.to_i + (60 * 10) # 10 minute expiration
+      exp: Time.now.to_i + (60 * 10) # 10 minute expiration
     }
     token = JWT.encode payload, ENV['METABASE_SECRET_KEY']
 
-    METABASE_SITE_URL + "/embed/question/" + token + "#bordered=true&titled=true"
+    METABASE_SITE_URL + '/embed/question/' + token + '#bordered=true&titled=true'
   end
 end
