e8aea1c6dd23f85cf649bf7f4cd11ea138efb8f0
Djamal Sadaghiani <dj8amal@gmail.com>
2021-01-17 00:12:39 +0100

    finished initial dashboard and added country code for metabase

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,5 +9,17 @@ class PagesController < ApplicationController
     redirect_to controller: :pages, action: :portfolio if user_signed_in?
   end
 
-  def portfolio; end
+  def portfolio
+    @portfolio_table = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 20, title: true)
+    @total_gain_absolut = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 21, title: false)
+    @total_gain_percentage = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 22, title: false)
+    @product_split = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 23, title: true)
+    @sector_split = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 24, title: true)
+    @type_split = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 25, title: true)
+    @product_performance = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 26, title: true)
+    @sector_performance = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 27, title: true)
+    @geo_split = MetabaseUrlCreator.call(user_id: current_user.id, question_id: 28, title: true)
+  end
 end
+
+
